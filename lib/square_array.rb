182c5214a8f81ea77ad6c900b666a0b1094fc08b
Naveed Ganjani <nganjani@protonmail.com>
2019-09-02 17:05:46 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,9 @@ def square_array(array)
   counter = 0
 
   while counter < array.length do
-    new_array = array[counter] ** 2
+    new_array[new_counter] = array[counter] ** 2
     counter += 1
   end
+  
   return new_array
 end
